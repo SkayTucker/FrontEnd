26336404857a7dfcf7ccc25dd111aabc51cebd93
unknown <Tramo-TI@DESKTOP-H13KVBT.TRAMO.local>
2023-08-19 15:23:59 -0300

    reset layout do site

--- makeIt/index.php
+++ makeIt/index.php
@@ -20,63 +20,29 @@
             </ul>
         </nav>
     </header>
-
-    <article class="etapas">
-            <p>Etapas do Desenvolvimento
-Configuração Inicial:
-
-Configurar um ambiente local de desenvolvimento (por exemplo, usando o XAMPP ou MAMP).
-Criar a estrutura básica do projeto (diretórios para HTML, CSS, JavaScript, PHP).
-Estabelecer conexão com o banco de dados MySQL.
-Sistema de Usuários:
-
-Criar a tabela de usuários no MySQL.
-Desenvolver o sistema de registro e login de usuários.
-Implementar a segurança necessária (por exemplo, hashing de senhas).
-Dashboard:
-
-Criar a interface principal após o login, onde o usuário pode ver seus projetos e eventos.
-Permitir a criação de novos projetos e eventos.
-Funcionalidades de Projeto:
-
-Criar a tabela de projetos e tarefas no MySQL.
-Desenvolver a interface para adicionar/editar/deletar projetos.
-Implementar a funcionalidade de subtópico-todolist dentro de cada projeto.
-Adicionar a área de discussão para cada tarefa.
-Integrações:
-
-Integrar com a API do Google Agenda para adicionar eventos.
-Implementar notificações por e-mail para alterações no projeto e notificações de eventos.
-Estilização e Design:
-
-Estilizar o projeto de forma semelhante ao Basecamp da 37signals.
-Refinar a experiência do usuário (UX).
-JavaScript:
-
-Implementar a interatividade na interface usando JavaScript. Como você mencionou que gostaria de aprender mais sobre isso, podemos usar exemplos simples e ir aumentando a complexidade conforme você se sinta confortável.</p>
-    </article>
-
-
     <main>
-    <div class="container">
+        <div class="container">
 
-    <section id="servicos">
-        <article id="mainCard" class="mainCard">
-            <h2>Crie seu Projeto</h2>
-            <p>Crie seu Projeto, Defina suas Prioridades e Prazos</p>
-        </article>
+            <div class="present">
 
-        <article id="editProject" class="mainCard">
-            <h2>Gerencie seu Projeto</h2>
-            <p>Defina Tarefas, Cargos e até como fazer as atividades</p>
-        </article>
+                <div class="formatCard">
+                    <h3>Crie Seu projeto do zero</h3>
+                    <p>Defina Tarefas, Cargos e até como fazer as atividades</p>
+                </div>
 
-        <article id="viewProject" class="mainCard">
-            <h2>Desenvolvimento do seu Projeto (Power BI ou Excel)</h2>
-            <p>Já pensou tudo bem explicadinho pra apresentar para o Cliente?</p>
-        </article>
-    </section>
-    </div>
+                <div class="formatCard">
+                <h2>Gerencie seu Projeto</h2>
+                    <p>Defina Tarefas, Cargos e até como fazer as atividades</p>
+                </div>
+
+                <div class="formatCard">
+                <h2>Desenvolvimento do seu Projeto (Power BI ou Excel)</h2>
+                    <p>Já pensou tudo bem explicadinho pra apresentar para o Cliente?</p>
+                </div>
+            </div>
+        </div>
+
+
     </main>
 
     <footer>
